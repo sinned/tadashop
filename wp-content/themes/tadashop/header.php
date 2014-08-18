580e45b03f8adc06545113b160b86ea378dc455b
Dennis Yang <sinned@gmail.com>
2014-08-18 14:40:46 -0700

    adding GA code

--- wp-content/themes/tadashop/header.php
+++ wp-content/themes/tadashop/header.php
@@ -13,6 +13,17 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-8792267-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <?php
 	if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' );
 	wp_head();
