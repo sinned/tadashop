cd6645234e90df665e01caddceb080d75b1fa2e0
Dennis Yang <sinned@gmail.com>
2014-10-08 15:31:24 -0700

    added wp popup plugin

---
+++ wp-content/plugins/m-wp-popup/wp-popup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+Plugin Name: WP Popup Plugin
+Plugin URI: http://rocketplugins.com/wordpress-popup-plugin/?ref=plugin_uri
+Description: The best WordPress Popup plugin. Period.
+Version: 0.5.3
+Author: Muneeb
+Author URI: http://rocketplugins.com/wordpress-popup-plugin/?ref=author_uri
+License: GPLv2 or later
+Copyright: 2013 Muneeb ur Rehman http://muneeb.me
+
+http://rocketplugins.com/contact
+**/
+
+require plugin_dir_path( __FILE__ ) . 'config.php';
+
+require POPUP_PLUGIN_INCLUDE_DIRECTORY . 'functions.php';
+
+load_wpp();
